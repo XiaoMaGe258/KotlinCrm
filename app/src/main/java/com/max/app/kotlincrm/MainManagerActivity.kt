3e5025d70b27fe3de8a7271370276863dc1debea
Max <9175289.qq.com>
2018-04-19 15:12:47 +0800

    完成首页

--- app/src/main/java/com/max/app/kotlincrm/MainManagerActivity.kt
+++ app/src/main/java/com/max/app/kotlincrm/MainManagerActivity.kt
@@ -1,29 +1,29 @@
 package com.max.app.kotlincrm
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ImageView
-import android.widget.TextView
+import android.support.v7.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import com.max.app.kotlincrm.adapter.TBaseAdapter
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
 import com.max.app.kotlincrm.api.JZBConstants
 import com.max.app.kotlincrm.ui.BaseActivity
 import com.max.app.kotlincrm.utils.GlideCircleTransform
 import com.max.app.kotlincrm.utils.Sp
 import kotlinx.android.synthetic.main.activity_main_manager.*
 import java.util.ArrayList
+import android.support.v7.widget.GridLayoutManager
+import android.view.View
+
 
 /**
  * 首页
  * Created by Xmg on 2018-4-18.
  */
-class MainManagerActivity : BaseActivity() {
+class MainManagerActivity : BaseActivity(), BaseQuickAdapter.OnItemClickListener {
 
     private val mGridViewList = ArrayList<GridViewItem>()
-
+//    var mAdapter = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_manager)
@@ -39,11 +39,33 @@ class MainManagerActivity : BaseActivity() {
         tv_user_role.text = Sp.get_String(JZBConstants.ROLENAME, "")
 
         initGridView()
-//        gv_grid.adapter = GridViewAdapter(mGridViewList, mContext)
-//        gv_grid.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-//            val item = parent.getItemAtPosition(position) as GridViewItem
-////            handlerGridViewItemOnclick(item.name)
-//        }
+        gv_grid.layoutManager = GridLayoutManager(this, 4)
+        val gridAdapter = GridViewAdapter(R.layout.home_gridview_item, mGridViewList)
+        gv_grid.adapter = gridAdapter
+        gridAdapter.onItemClickListener = this
+
+    }
+
+    override fun onItemClick(adapter: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+        val item = adapter!!.getItem(position) as GridViewItem
+        handlerGridViewItemOnclick(item.name)
+    }
+
+    private fun handlerGridViewItemOnclick(name: String) {
+        when(name){
+            "个人客户池" -> {
+//            PrivateLibraryActivity.actionPrivateLibraryActivity(mContext)
+            }
+            "公共客户池" -> {
+//            PublicLibraryActivity.actionPublicLibraryActivity(mContext)
+            }
+            "付费客户池" -> {
+//            PayMoneyActivity.actionPayMoneyActivity(mContext)
+            }
+            "添加客户" -> {
+//            UnfamiliarVisitedActivity.actionUnfamiliarVisitedActivity(mContext, false)
+            }
+        }
     }
 
     private fun initGridView(){
@@ -53,29 +75,19 @@ class MainManagerActivity : BaseActivity() {
         mGridViewList.add(GridViewItem("添加客户", R.drawable.ic_unfamiliar_visit))
     }
 
-    private inner class GridViewItem
+    inner class GridViewItem
     @JvmOverloads constructor(
             internal var name: String,
-            internal var icon: Int,
-            internal var showRedPoint: Boolean = false
+            internal var icon: Int
     )
 
-    private inner class GridViewAdapter(list: List<GridViewItem>, context: Activity) : TBaseAdapter<GridViewItem>(context, list) {
-        override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
-            var convertView = convertView
-            convertView = mLayoutInflater.inflate(R.layout.home_gridview_item, parent, false)
-            val nameText = convertView.findViewById(R.id.grid_view_item_name) as TextView
-            val iconImage = convertView.findViewById(R.id.grid_view_item_image) as ImageView
-            val item = getItem(position) as GridViewItem
-            nameText.text = item.name
-            iconImage.setImageResource(item.icon)
+    inner class GridViewAdapter(layoutResId: Int, data: List<GridViewItem>) : BaseQuickAdapter<GridViewItem, BaseViewHolder>(layoutResId, data) {
 
-            if ("" != item.name) {
-                convertView.setBackgroundResource(R.drawable.gridview_select)
-            } else {
-                convertView.setBackgroundResource(R.drawable.gridview_select_no_use)
-            }
-            return convertView
+        override fun convert(helper: BaseViewHolder, item: GridViewItem) {
+            helper.setText(R.id.grid_view_item_name, item.name)
+            helper.setImageResource(R.id.grid_view_item_image, item.icon)
         }
     }
+
+
 }
